1fbde6e35ce054a1b4ee79ba0cd7803702244fc1
ju <thomas.julien@protonmail.com>
2026-01-11 14:57:43 +0100

    Add some untracked changes 'cause i'm lazy

--- config.def.h
+++ config.def.h
@@ -20,7 +20,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 12;
 
 /*
  * Characters not considered part of a word while deleting words
